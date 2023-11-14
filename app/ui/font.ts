6146505986e613fa44a9a554174c5a0891b5386a
Mauricio Teruel <mauricioteruel98@gmail.com>
2023-11-13 23:09:03 -0300

    Firts commit

---
+++ app/ui/font.ts
@@ -0,0 +1,5 @@
+import { Acme, Inter, Montserrat, Lusitana } from 'next/font/google'
+
+export const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '700']})
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
